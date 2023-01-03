fbaa8359dd0050ba89ac0045e9ef6148bfecfbce
phandyphorn <phornphandy20@gmail.com>
2023-01-03 14:40:01 +0700

    devServer

--- vue.config.js
+++ vue.config.js
@@ -10,6 +10,18 @@ module.exports = defineConfig({
     vuetify: {
 			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
 		}
+  },
+
+  devServer: {
+    proxy: {
+      '^/api': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true
+      },
+    }
   }
+
+
+  
 })
 
